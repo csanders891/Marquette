d8401ac21014f90f28c9baa2c17765138e7813db
csanders891 <65356624+csanders891@users.noreply.github.com>
2020-07-23 00:03:57 -0500

    Add files via upload

---
+++ src/marquette/Marquette.java
@@ -0,0 +1,38 @@
+package marquette;
+
+import java.util.ArrayList;
+
+public class Marquette {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList <String> teammates=new ArrayList<String>();
+		System.out.println("Hello.  I'm Carter and these are my Marquette Park teammates:");
+		
+		teammates.add("Lavitta");
+		teammates.add("T.J.");
+		teammates.add("Byron");
+		teammates.add("Charles");
+		teammates.add("Chris");
+		teammates.add("Sylvia");
+		teammates.add("Jane");
+		teammates.add("Bernard");
+		teammates.add("Arthur");
+		teammates.add("Brandon");
+		teammates.add("Laurie");
+		teammates.add("Janet");
+		teammates.add("Deanna");
+		teammates.add("Ellen");
+		teammates.add("Maria");
+		teammates.add("Tysheika");
+		teammates.add("Marcus");
+		teammates.add("Jason");
+		teammates.add("Renee");
+		
+		for(int i=0; i < teammates.size(); i++)
+			System.out.println(teammates.get(i));
+		
+		
+	}
+
+}
